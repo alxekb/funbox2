9977b29d7b7aa3abe1c43054c0fc7d6903bed2f3
AlexIv <alx.ekb@gmail.com>
2019-08-31 17:34:07 +0500

    Webpack environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+["css", "moduleCss"].forEach(loaderName => {
+  const loader = environment.loaders.get(loaderName);
 
-module.exports = environment
+  loader.test = /\.(p?css)$/i;
+
+  environment.loaders.insert(loaderName, loader);
+});
+
+module.exports = environment;
